d40d3d14596e465c79501912dcadddd1065a3b13
Tobias Sterbak <sterbak-it@outlook.com>
2021-09-14 13:47:52 +0200

    Add new source laws; Integrate change law title identification and change law segmentation code in main script

--- pre_law_viewer/generate_diff.py
+++ pre_law_viewer/generate_diff.py
@@ -1,10 +1,12 @@
-"""Main script to generate diffs from pairs (change law pdf, source law text).
+"""Main script to generate diffs from change law pdf.
 
 Example usage:
-    poetry run python ./pre_law_viewer/generate_diff.py -c data/0483-21.pdf -s data/0483-21_wettbewerbsgesetzt.txt
+    poetry run python ./pre_law_viewer/generate_diff.py -c data/0483-21.pdf
 """
+import os
 import click
 from pre_law_viewer.parsing.change_law_utils import read_pdf_law, preprocess_raw_law, expand_text
+from pre_law_viewer.parsing.change_law_utils import extract_raw_proposal, extract_seperate_change_proposals, extract_law_titles, remove_inkrafttreten
 from pre_law_viewer.parsing.parse_change_law import parse_change_request_line
 from pre_law_viewer.parsing.parse_source_law import parse_source_law_tree, LawTextNode
 from pre_law_viewer.apply_changes.apply_changes import apply_changes
@@ -12,33 +14,59 @@ from pre_law_viewer.apply_changes.apply_changes import apply_changes
 
 @click.command()
 @click.option('change_law_path', '-c', help='Path to the change law pdf.', type=click.Path(exists=True))
-@click.option('source_law_path', '-s', help='Path to the source law txt.', type=click.Path(exists=True))
-def generate_diff(change_law_path, source_law_path):
+#@click.option('source_law_path', '-s', help='Path to the source law txt.', type=click.Path(exists=True))
+@click.option('output_path', '-o', help='Where to write the output (logs and modified laws).', default="./output/")
+def generate_diff(change_law_path, output_path):
     """Generate the diff from the change law and the source law."""
     click.echo("Started parsing {}".format(change_law_path))
-    # 1. read and format the change requests
+    # read the change law 
     change_law_raw = read_pdf_law(change_law_path)
-    clean_change_law = preprocess_raw_law(change_law_raw)
-    clean_text = expand_text(clean_change_law)
     
-    # 2 parse the change requests in a structured format
-    change_requests = []
-    for change_request_line in clean_text.split("\n"):
-        res = parse_change_request_line(change_request_line)
-        change_requests.extend(res)
+    # idenfify the different laws affected
+    change_law_extract = extract_raw_proposal(change_law_raw)
+    proposals_list = extract_seperate_change_proposals(change_law_extract)
+    law_titles = extract_law_titles(proposals_list)
+    law_titles, proposals_list = remove_inkrafttreten(law_titles, proposals_list)
+    
+    # parse and apply changes for every law that should be changed
+    for law_title, change_law in zip(law_titles, proposals_list):
+        # find and load the source law
+        source_law_path = "data/source_laws/{}.txt".format(law_title)
+        try:
+            with open(source_law_path, "r") as f:
+                source_law_text = f.read()
+            click.echo("Apply changes to {}".format(law_title))
+        except:
+            click.echo("Cannot find source law {}. SKIPPING".format(law_title))
+            continue
         
-    # 3. load and parse source law
-    source_law_text = open(source_law_path, "r").read()
-    parsed_law_tree = parse_source_law_tree(
-        text=source_law_text
-    )
-    
-    # 4 apply changes to the source law
-    res_law_tree = apply_changes(parsed_law_tree, change_requests)
+        # format the change requests
+        clean_change_law = preprocess_raw_law(change_law)
+        clean_text = expand_text(clean_change_law)
 
-    # 5. save final version to file
-    with open('output.txt', 'w') as f:
-        f.write(res_law_tree._to_text())
+        # parse the change requests in a structured format
+        change_requests = []
+        for change_request_line in clean_text.split("\n"):
+            res = parse_change_request_line(change_request_line)
+            change_requests.extend(res)
+
+        # parse source law
+        
+        parsed_law_tree = parse_source_law_tree(
+            text=source_law_text
+        )
+
+        # apply changes to the source law
+        res_law_tree = apply_changes(parsed_law_tree, change_requests)
+
+        #  save final version to file
+        write_path = "{}{}_modified_{}.txt".format(output_path, law_title, change_law_path.split("/")[-1])
+        click.echo("Write results to {}".format(write_path))
+        if not os.path.exists(output_path):
+            os.makedirs(output_path)
+
+        with open(write_path, 'w') as f:
+            f.write(res_law_tree._to_text())
     click.echo("DONE.")
     
     
