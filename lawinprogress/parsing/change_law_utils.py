772a069c9caedd1ea906d38acf7c34ff73344229
Tobias Sterbak <sterbak-it@outlook.com>
2021-11-17 15:04:36 +0100

    add unittest for cleaning up change laws.

--- lawinprogress/parsing/change_law_utils.py
+++ lawinprogress/parsing/change_law_utils.py
@@ -39,6 +39,45 @@ def remove_newline_in_quoted_text(text: str) -> str:
     return text
 
 
+def clean_line(line: str):
+    """Apply some cleaning steps to a line of change law text.strip
+
+    Args:
+        line: String of a line
+
+    Returns:
+        cleaned line
+    """
+    line = line.strip()
+
+    # if a line starts with a single digit, we suppose its a page number and remove it
+    line = re.sub(r"^\d{1,2}\s", "", line)
+    # line = re.sub(r"- \d -", "", line)
+
+    #  remove drucksache page break stuff
+    drucksache_regexs = [
+        r"\sDeutscher\s{1,5}Bundestag\s{1,5}\S\s{1,5}\d{1,2}\.\s{1,3}Wahlperiode\s{1,5}\S\s{1,5}\d{1,3}\s{1,5}\S\s{1,5}Drucksache\s{1,5}\d{1,3}\/\d{1,7}",
+        r"\sDrucksache\s{1,5}\d{1,3}\/\d{1,7}\s{1,5}\S\s{1,5}\d{1,2}\s{1,5}\S\s{1,5}Deutscher\s{1,3}Bundestag\s{1,5}\S\s{1,5}\d{1,2}\.\s{1,3}Wahlperiode\s",
+    ]
+    for drucksache_regex in drucksache_regexs:
+        line = re.sub(drucksache_regex, "", line)
+    return line
+
+
+def remove_footnotes(text: str):
+    """Remove footnotes from the full text of the change law.
+
+    Args:
+        text: Text of the law.
+
+    Returns:
+        Text without footnotes.
+    """
+    # try to remove footnotes
+    # Inspired by the footnote on page 7 in 1928399.pdf
+    return re.sub(r"\*(.|\n)*?Wahlperiode\s", "", text)
+
+
 def preprocess_raw_law(text: str) -> str:
     """Apply some preprocessing to the raw text of the laws.
 
@@ -60,33 +99,20 @@ def preprocess_raw_law(text: str) -> str:
         r"wird[\s,\n]{0,3}wie[\s,\n]{0,3}folgt[\s,\n]{0,3}geändert:", text, maxsplit=1
     )[1].split("Begründung", 1)[0]
 
+    # remove footnotes
+    text = remove_footnotes(text)
+
     # Remove newlines between quotation marks
     text = remove_newline_in_quoted_text(text)
-    
-    # try to remove footnotes
-    # Inpired by the footnote on page 7 in 1928399.pdf
-    text = re.sub(r"\s\*(.|\n)*?Wahlperiode\s", "", text)
 
-    # remove more text artifacts from the page
-    # text = re.sub(r"- \d -", "", text)  # page numbering
     text = text.strip()  # remove trailing whitespace or newlines
-    
+
     # pull every bulletpoint content to one line
     outtext = ""
     for line in text.split("\n"):
-        line = line.strip()
-        # if a line starts with a single digit, we suppose its a page number and remove it
-        line = re.sub(r"^\d{1,2}\s", "", line)
-        #  remove drucksache page break stuff
-        drucksache_regexs = [
-            r"\sDeutscher\s{1,5}Bundestag\s{1,5}\S\s{1,5}\d{1,2}\.\s{1,3}Wahlperiode\s{1,5}\S\s{1,5}\d{1,3}\s{1,5}\S\s{1,5}Drucksache\s{1,5}\d{1,3}\/\d{1,7}",
-            r"\sDrucksache\s{1,5}\d{1,3}\/\d{1,7}\s{1,5}\S\s{1,5}\d{1,2}\s{1,5}\S\s{1,5}Deutscher\s{1,3}Bundestag\s{1,5}\S\s{1,5}\d{1,2}\.\s{1,3}Wahlperiode\s",
-        ]
-        for drucksache_regex in drucksache_regexs:
-            line = re.sub(drucksache_regex, "", line)
-        # remove lines with a footnote star *
-        #if any(pattern in line for pattern in ["*", "Drucksache"]):
-        #    line = ""
+        # apply some cleaning
+        line = clean_line(line)
+
         # check if line starts with a bullet point identifier
         # > if yes, put it in a new line, otherwise just append the linetext to the text
         if any(
--- tests/test_change_law_utils.py
+++ tests/test_change_law_utils.py
@@ -1,7 +1,12 @@
 """Test basic cleaning functions for change laws."""
 import pytest
 
-from lawinprogress.parsing.change_law_utils import remove_newline_in_quoted_text, preprocess_raw_law
+from lawinprogress.parsing.change_law_utils import (
+    clean_line,
+    preprocess_raw_law,
+    remove_footnotes,
+    remove_newline_in_quoted_text,
+)
 
 
 def test_remove_newline_in_quoted_text_simple_quotes():
@@ -24,9 +29,39 @@ def test_remove_newline_in_quoted_text_nested_quotes():
 
 def test_remove_newline_in_quoted_text_uneven_numbered_quotes():
     """Test if the function raises an exception if the number of opening and closing quotes is not equal."""
-    test_text = "The following\n text „is \nin quotes“. „This is missing closing quotes."
+    test_text = (
+        "The following\n text „is \nin quotes“. „This is missing closing quotes."
+    )
 
     with pytest.raises(Exception) as err:
         result_text = remove_newline_in_quoted_text(test_text)
 
     assert str(err.value) == "Something with the quotes is wrong."
+
+
+def test_remove_footnotes():
+    """Test if footnotes are correctly removed."""
+    text_with_footnote = """genutzten  Postfach- und  Versanddienst  eines  Nutzerkontos  im  Sinne des  § 2  Absatz 5 des
+Onlinezugangsgesetzes  und  der  elektronischen Poststelle des Gerichts,“.
+*   Notifiziert  gemäß  der  Richtlinie  (EU)  2015/1535  des  Europäischen  Parlaments  und  des Rates vom  9.  September  2015  über  ein  Informationsver-
+fahren  auf  dem Gebiet  der  technischen  Vorschriften  und  der  Vorschriften  für  die  Dienste  der  Informati-onsgesellschaft  (ABl.  L  241  vom
+17.9.2015,  S.  1).  Drucksache  19/28399  – 8 –   Deutscher  Bundestag – 19. Wahlperiode
+cc)  Die * bisherige Nummer 4 wird Nummer 6.
+dd)  Folgender Satz wird angefügt:"""
+
+    text_without_footnote = """genutzten  Postfach- und  Versanddienst  eines  Nutzerkontos  im  Sinne des  § 2  Absatz 5 des
+Onlinezugangsgesetzes  und  der  elektronischen Poststelle des Gerichts,“.
+cc)  Die * bisherige Nummer 4 wird Nummer 6.
+dd)  Folgender Satz wird angefügt:"""
+
+    print(remove_footnotes(text_with_footnote))
+
+    assert text_without_footnote == remove_footnotes(text_with_footnote)
+
+
+def test_clean_line():
+    """Test if a line of change law text can be properly cleaned."""
+    test_line = """(2)  Eine Zustellung gegen Empfangsbekenntnis  kann auch  durch Telekopie  erfolgen.  Die  Übermittlung soll  mit dem Hinweis  „Zustellung gegen Empfangsbekenntnis“ eingeleitet werden und die absendende  Deutscher  Bundestag – 19. Wahlperiode   – 9 –   Drucksache  19/28399 Stelle, den  Namen und  die Anschrift  des  Zustellungsadressaten sowie den  Namen  des  Justizbediensteten"""
+    clean_test_line = """(2)  Eine Zustellung gegen Empfangsbekenntnis  kann auch  durch Telekopie  erfolgen.  Die  Übermittlung soll  mit dem Hinweis  „Zustellung gegen Empfangsbekenntnis“ eingeleitet werden und die absendende  Stelle, den  Namen und  die Anschrift  des  Zustellungsadressaten sowie den  Namen  des  Justizbediensteten"""
+
+    assert clean_test_line == clean_line(test_line)
