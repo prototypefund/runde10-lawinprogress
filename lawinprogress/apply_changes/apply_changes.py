e4c47bda72dac58a550cfe3032659a4151443a3d
Tobias Sterbak <sterbak-it@outlook.com>
2021-11-24 18:18:21 +0100

    Add more information if applying a change doesn't accutally change anything

--- lawinprogress/apply_changes/apply_changes.py
+++ lawinprogress/apply_changes/apply_changes.py
@@ -88,28 +88,24 @@ def apply_changes(law_tree: LawTextNode, changes: List[Change]) -> LawTextNode:
         ]
         # find the node that needs to be changed
         node = _find_node(location_list=location_list, parse_tree=res_law_tree)
+        # store the representation of the node to compare it with the node after the change
+        node_text_before = node.to_text()
         # if we found no path, we skip
         if not node:
             print("No path found in {}. SKIPPING".format(change))
             continue
         change_type = change.change_type
         if change_type == "replace":
-            print("APPLIED {}: {}".format(change_type, change))
             status = _replace(node, change)
         elif change_type == "insert_after":
-            print("APPLIED {}: {}".format(change_type, change))
             status = _insert_after(node, change)
         elif change_type == "rephrase":
-            print("APPLIED {}:\n{}".format(change_type, change))
             status = _rephrase(node, change)
         elif change_type == "append":
-            print("APPLIED {}:\n{}".format(change_type, change))
             status = _append(node, change)
         elif change_type == "delete_after":
-            print("APPLIED {}: {}".format(change_type, change))
             status = _delete_after(node, change)
         elif change_type == "cancelled":
-            print("APPLIED {}: {}".format(change_type, change))
             status = _cancelled(node, change)
         elif change_type == "RENUMBERING":
             print("SKIPPED {}: {}".format(change_type, change))
@@ -117,7 +113,14 @@ def apply_changes(law_tree: LawTextNode, changes: List[Change]) -> LawTextNode:
             status = 1
         else:
             print("SKIPPED {}: {}".format(change_type, change))
-        n_succesfull_applied_changes += status
+        if node.to_text() != node_text_before:
+            # if something changed, then we successfully applied something
+            print("APPLIED {}: {}".format(change_type, change))
+            n_succesfull_applied_changes += status
+        else:
+            # if nothign changed, we should be informed
+            if change_type != "RENUMBERING":
+                print("WITHOUT CHANGE {}: {}".format(change_type, change))
     # print a status update
     print(
         "\nSuccessfully applied {} out of {} changes ({:.1%})\n".format(
