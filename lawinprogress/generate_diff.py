900ae43cecb12b7a1c49501ce980885a28df7f1e
Tobias Sterbak <sterbak-it@outlook.com>
2022-01-07 09:58:58 +0000

    Refactor generate_diff in different functions for processing the pdfs and processing and applying the changes.

--- lawinprogress/generate_diff.py
+++ lawinprogress/generate_diff.py
@@ -4,16 +4,18 @@ Example usage:
     poetry run python ./lawinprogress/generate_diff.py -c data/0483-21.pdf --html
 """
 import os
+from typing import List, Tuple
 
 import click
 import outputformat as ouf
 from anytree import PreOrderIter
 
-from lawinprogress.apply_changes.apply_changes import apply_changes
+from lawinprogress.apply_changes.apply_changes import ChangeResult, apply_changes
 from lawinprogress.libdiff.html_diff import html_diffs
 from lawinprogress.parsing.change_law_utils import preprocess_raw_law
 from lawinprogress.parsing.lawtree import LawTextNode
 from lawinprogress.parsing.parse_change_law import (
+    Change,
     parse_change_law_tree,
     parse_change_request_line,
 )
@@ -25,6 +27,90 @@ from lawinprogress.parsing.proposal_pdf_to_artikles import (
     read_pdf_law,
     remove_inkrafttreten,
 )
+
+
+def process_pdf(change_law_path: str) -> Tuple[List[str], List[str]]:
+    """Wrapper function to process pdf of change law.
+
+    Args:
+      change_law_path: Path to the pdf in question.
+
+    Returns:
+      List of law titles affected by the change law.
+      List of texts of the change requests.
+    """
+    # read the change law
+    change_law_raw = read_pdf_law(change_law_path)
+
+    # idenfify the different laws affected
+    change_law_extract = extract_raw_proposal(change_law_raw)
+    proposals_list = extract_seperate_change_proposals(change_law_extract)
+    law_titles = extract_law_titles(proposals_list)
+    law_titles, proposals_list = remove_inkrafttreten(law_titles, proposals_list)
+    return law_titles, proposals_list
+
+
+def parse_and_apply_changes(
+    change_law_text: str, source_law_text: str, law_title: str, loglevel: int
+) -> Tuple[LawTextNode, LawTextNode, List[Change], List[ChangeResult], int]:
+    """Wrapper function to parse and apply changes from the change law text to a source law.
+
+    Args:
+      change_law_text: Text of the change law.
+      source_law_text: Text of the affected source law.
+      law_title: Title of the affected law.
+      loglevel: Integer indicating the loglevel.
+
+    Returns:
+      Tree of LawTextNodes of the law before the changes applied.
+      Tree of LawTextNodes of the law after the changes applied.
+      List of requested Changes.
+      List of change results.
+      Number of successfully applied changes.
+    """
+    # format the change requests and parse them to tree
+    clean_change_law = preprocess_raw_law(change_law_text)
+    parsed_change_law_tree = LawTextNode(text=law_title, bulletpoint="change")
+    parsed_change_law_tree = parse_change_law_tree(
+        text=clean_change_law, source_node=parsed_change_law_tree
+    )
+
+    # parse the change requests in a structured line format
+    all_change_lines = []
+    # collect all paths to tree leaves and join them in the right order
+    for leaf_node in parsed_change_law_tree.leaves:
+        path = [str(leaf_node)]
+        node = leaf_node
+        while node.parent:
+            node = node.parent
+            path.append(str(node))
+        change_line = " ".join(path[::-1][1:])
+        all_change_lines.append(change_line)
+
+    # parse the change request lines to changes
+    change_requests = []
+    for change_request_line in all_change_lines:
+        res = parse_change_request_line(change_request_line)
+        if res:
+            change_requests.extend(res)
+
+    # parse source law
+    parsed_law_tree = LawTextNode(text=law_title, bulletpoint="source")
+    parsed_law_tree = parse_source_law_tree(
+        text=source_law_text, source_node=parsed_law_tree
+    )
+
+    # apply changes to the source law
+    res_law_tree, change_results, n_succesfull_applied_changes = apply_changes(
+        parsed_law_tree, change_requests, loglevel
+    )
+    return (
+        parsed_law_tree,
+        res_law_tree,
+        change_requests,
+        change_results,
+        n_succesfull_applied_changes,
+    )
 
 
 @click.command()
@@ -60,17 +146,12 @@ def generate_diff(change_law_path: str, output_path: str, loglevel: int, html: b
     ouf.bigtitle("Law in Progress")
     click.echo(ouf.boxtitle(f"Started parsing {change_law_path}", return_str=True))
     click.echo("\n" + "#" * 150 + "\n")
-    # read the change law
-    change_law_raw = read_pdf_law(change_law_path)
 
-    # idenfify the different laws affected
-    change_law_extract = extract_raw_proposal(change_law_raw)
-    proposals_list = extract_seperate_change_proposals(change_law_extract)
-    law_titles = extract_law_titles(proposals_list)
-    law_titles, proposals_list = remove_inkrafttreten(law_titles, proposals_list)
+    # process the pdf
+    law_titles, proposals_list = process_pdf(change_law_path)
 
     # parse and apply changes for every law that should be changed
-    for law_title, change_law in zip(law_titles, proposals_list):
+    for law_title, change_law_text in zip(law_titles, proposals_list):
         # find and load the source law
         source_law_path = f"data/source_laws/{law_title}.txt"
         try:
@@ -80,45 +161,19 @@ def generate_diff(change_law_path: str, output_path: str, loglevel: int, html: b
         except FileNotFoundError as err:
             click.echo(f"Cannot find source law {law_title}. SKIPPING")
             continue
-
         click.echo("\n" + "#" * 150 + "\n")
 
-        # format the change requests and parse them to tree
-        clean_change_law = preprocess_raw_law(change_law)
-        parsed_change_law_tree = LawTextNode(text=law_title, bulletpoint="change")
-        parsed_change_law_tree = parse_change_law_tree(
-            text=clean_change_law, source_node=parsed_change_law_tree
+        # Parse the source and change law and apply the requested changes.
+        (
+            parsed_law_tree,
+            res_law_tree,
+            change_requests,
+            change_results,
+            n_succesfull_applied_changes,
+        ) = parse_and_apply_changes(
+            change_law_text, source_law_text, law_title, loglevel
         )
 
-        # parse the change requests in a structured line format
-        all_change_lines = []
-        # collect all paths to tree leaves and join them in the right order
-        for leaf_node in parsed_change_law_tree.leaves:
-            path = [str(leaf_node)]
-            node = leaf_node
-            while node.parent:
-                node = node.parent
-                path.append(str(node))
-            change_line = " ".join(path[::-1][1:])
-            all_change_lines.append(change_line)
-
-        # parse the change request lines to changes
-        change_requests = []
-        for change_request_line in all_change_lines:
-            res = parse_change_request_line(change_request_line)
-            if res:
-                change_requests.extend(res)
-
-        # parse source law
-        parsed_law_tree = LawTextNode(text=law_title, bulletpoint="source")
-        parsed_law_tree = parse_source_law_tree(
-            text=source_law_text, source_node=parsed_law_tree
-        )
-
-        # apply changes to the source law
-        res_law_tree, change_results, n_succesfull_applied_changes = apply_changes(
-            parsed_law_tree, change_requests, loglevel
-        )
         # print a status updatei
         result_status = ouf.bar(
             n_succesfull_applied_changes,
@@ -159,8 +214,8 @@ def generate_diff(change_law_path: str, output_path: str, loglevel: int, html: b
                 applied_change_results,
             )
             # save to fiel
-            diff_write_path = "{}{}_diff_{}.html".format(
-                output_path, law_title, change_law_path.split("/")[-1]
+            diff_write_path = (
+                f"{output_path}{law_title}_diff_{change_law_path.split('/')[-1]}.html"
             )
             with open(diff_write_path, "w", encoding="utf8") as file:
                 file.write(html_side_by_side)
