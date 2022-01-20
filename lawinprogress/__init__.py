361fda1e83befb507bcf7bf5a00c1d7d6b3d968b
Tobias Sterbak <sterbak-it@outlook.com>
2022-01-20 13:59:34 +0000

    Reduce size of spacy pipeline

--- lawinprogress/__init__.py
+++ lawinprogress/__init__.py
@@ -1,5 +1,8 @@
 """Preload spacy and model here, to reuse and speed up the process."""
 import spacy
 
-NLP = spacy.load("de_core_news_sm", exclude=["tok2vec", "tagger", "ner", "parser"])
+NLP = spacy.load(
+    "de_core_news_sm",
+    exclude=["tok2vec", "tagger", "morphologizer", "parser", "attribute_ruler", "lemmatizer", "ner"]
+)
 NLP.enable_pipe("senter")
