9f39e8a0b205237cac694c143c3820f36cad0f26
Tobias Sterbak <sterbak-it@outlook.com>
2022-01-20 14:05:30 +0000

    Update port and host

--- lawinprogress/app/html.py
+++ lawinprogress/app/html.py
@@ -55,52 +55,59 @@ async def generate_diff(request: Request, change_law_pdf: UploadFile = Form(...)
 
     Return the result.
     """
-    law_titles, proposals_list = process_pdf(change_law_pdf.file)
-    logger.info(f"Processing {change_law_pdf.filename}...")
+    try:
+        law_titles, proposals_list = process_pdf(change_law_pdf.file)
+        logger.info(f"Processing {change_law_pdf.filename}...")
 
-    results = []
-    for law_title, change_law_text in zip(law_titles, proposals_list):
-        logger.info(f"Started processing change for {law_title}...")
-        # find and load the source law
-        source_law_path = f"data/source_laws/{law_title}.txt"
-        try:
-            with open(source_law_path, "r", encoding="utf8") as file:
-                source_law_text = file.read()
-        except FileNotFoundError as err:
-            results.append("<p>Source law not found.</p>")
-            continue
+        results = []
+        for law_title, change_law_text in zip(law_titles, proposals_list):
+            logger.info(f"Started processing change for {law_title}...")
+            # find and load the source law
+            source_law_path = f"data/source_laws/{law_title}.txt"
+            try:
+                with open(source_law_path, "r", encoding="utf8") as file:
+                    source_law_text = file.read()
+            except FileNotFoundError as err:
+                results.append("<p>Source law not found.</p>")
+                continue
 
-        # Parse the source and change law and apply the requested changes.
-        (
-            parsed_law_tree,
-            res_law_tree,
-            change_requests,
-            change_results,
-            n_succesfull_applied_changes,
-        ) = parse_and_apply_changes(
-            change_law_text,
-            source_law_text,
-            law_title,
+            # Parse the source and change law and apply the requested changes.
+            (
+                parsed_law_tree,
+                res_law_tree,
+                change_requests,
+                change_results,
+                n_succesfull_applied_changes,
+            ) = parse_and_apply_changes(
+                change_law_text,
+                source_law_text,
+                law_title,
+            )
+
+            # generate the html diff
+            applied_change_results = [
+                node.changes for node in PreOrderIter(res_law_tree) if node.changes
+            ]
+            # get the diff
+            html_side_by_side = html_diffs(
+                parsed_law_tree.to_text(),
+                res_law_tree.to_text(),
+                applied_change_results,
+            )
+            results.append(html_side_by_side)
+
+        # prepare the html output and return it
+        result = list(zip(law_titles, results))
+        return templates.TemplateResponse(
+            "results_index.html",
+            context={"request": request, "result": result, "name": change_law_pdf.filename},
         )
-
-        # generate the html diff
-        applied_change_results = [
-            node.changes for node in PreOrderIter(res_law_tree) if node.changes
-        ]
-        # get the diff
-        html_side_by_side = html_diffs(
-            parsed_law_tree.to_text(),
-            res_law_tree.to_text(),
-            applied_change_results,
+    except Exception as err:
+        logger.info(err)
+        return templates.TemplateResponse(
+            "errorpage.html",
+            context={"request": request}
         )
-        results.append(html_side_by_side)
-
-    # prepare the html output and return it
-    result = list(zip(law_titles, results))
-    return templates.TemplateResponse(
-        "results_index.html",
-        context={"request": request, "result": result, "name": change_law_pdf.filename},
-    )
 
 
 @app.get("/imgs/{image_name}")
